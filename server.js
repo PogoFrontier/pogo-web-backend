470e3ee1ba0e359b38d60c7d6c3250428b22faf7
Adib Khan <adib.khan@gmail.com>
2020-11-24 01:43:52 -0500

    add server

---
+++ server.js
@@ -0,0 +1,39 @@
+
+const
+    http = require("http"),
+    express = require("express"),
+    socketio = require("socket.io");
+
+const SERVER_PORT = 3000;
+
+let onlineClients = new Set();
+
+function onNewWebsocketConnection(socket) {
+    console.info(`Socket ${socket.id} has connected.`);
+    onlineClients.add(socket.id);
+
+    socket.on("disconnect", () => {
+        onlineClients.delete(socket.id);
+        console.info(`Socket ${socket.id} has disconnected.`);
+    });
+}
+
+function startServer() {
+    // create a new express app
+    const app = express();
+    // create http server and wrap the express app
+    const server = http.createServer(app);
+    // bind socket.io to that server
+    const io = socketio(server);
+
+    // serve static files from a given folder
+    app.use(express.static("public"));
+
+    // will fire for every new websocket connection
+    io.on("connection", onNewWebsocketConnection);
+
+    // important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
+    server.listen(SERVER_PORT, () => console.info(`Listening on port ${SERVER_PORT}.`));
+}
+
+startServer();
