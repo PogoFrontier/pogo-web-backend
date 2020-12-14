91d1843b12a41908b80e483ab48aaf4a1e965b39
Adib Khan <adib.khan@gmail.com>
2020-12-13 19:50:51 -0500

    refactor to action

---
+++ actions/to.ts
@@ -0,0 +1,13 @@
+import { onlineClients, rooms } from "../server";
+
+function to(room: string, data: string, id?: string) {
+  if (rooms.get(room)) {
+      for (let player of rooms.get(room)!.players) {
+          if (player && player.id !== id) {
+              onlineClients.get(player.id)!.send(data);
+          }
+      }
+  }
+}
+
+export default to;
