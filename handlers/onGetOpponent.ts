84c8cdaccb496650b50b54da6c7517b1394cabe7
Adib Khan <adib.khan@gmail.com>
2020-12-13 19:51:42 -0500

    refactor onGetOpponent

---
+++ handlers/onGetOpponent.ts
@@ -0,0 +1,25 @@
+import to from "../actions/to";
+import { rooms } from "../server";
+import { CODE } from "../types/actions";
+
+export interface OnGetOpponentPayload {
+  room: string,
+}
+
+function onGetOpponent(id: string, payload: OnGetOpponentPayload) {
+  const { room } = payload;
+  const currentRoom = rooms.get(room);
+  if (currentRoom) {
+      const opp = currentRoom.players.find((x) => (x !== null) && (x.id !== id))
+      if (opp) {
+          to(room, JSON.stringify({
+              type: CODE.room_join,
+              payload: { team: opp.team }
+          }), id)
+      } else {
+          console.error("No opponent found");
+      }
+  }
+}
+
+export default onGetOpponent;
