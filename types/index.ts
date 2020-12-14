281e22b4d8bfbfe0bed9523af8c9b8ac9bbe4096
Adib Khan <adib.khan@gmail.com>
2020-12-13 22:23:04 -0500

    add types publisher

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+import { CODE, Actions } from './actions'
+import { RoomStatus, Room, Player, TurnAction } from './room'
+import { TeamMember, Current } from './team'
+import { OnGetOpponentPayload, OnNewRoomPayload, OnReadyGamePayload, OnTeamSubmitPayload } from './handlers'
+
+export {
+  CODE,
+  Actions,
+  RoomStatus,
+  Room,
+  Player,
+  TurnAction,
+  TeamMember,
+  Current,
+  OnGetOpponentPayload,
+  OnNewRoomPayload,
+  OnReadyGamePayload,
+  OnTeamSubmitPayload
+}
