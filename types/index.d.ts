e2204f31baf4417fed57cc0b6a287bfb47f324e9
Adib Khan <adib.khan@gmail.com>
2020-12-16 14:01:49 -0500

    export types

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,6 +11,8 @@ export interface Player extends room.Player {
 }
 export interface Room extends room.Room {
 }
+export interface Move extends room.Move {
+}
 export interface OnGetOpponentPayload extends handlers.OnGetOpponentPayload {
 }
 export interface OnNewRoomPayload extends handlers.OnNewRoomPayload {
@@ -18,4 +20,10 @@ export interface OnNewRoomPayload extends handlers.OnNewRoomPayload {
 export interface OnReadyGamePayload extends handlers.OnReadyGamePayload {
 }
 export interface OnTeamSubmitPayload extends handlers.OnTeamSubmitPayload {
+}
+export interface OnActionProps extends handlers.OnActionProps {
+}
+export interface ResolveTurnPayload extends handlers.ResolveTurnPayload {
+}
+export interface Update extends handlers.Update {
 }
--- types/index.ts
+++ types/index.ts
@@ -6,8 +6,11 @@ export interface Current extends team.Current {}
 export interface TurnAction extends room.TurnAction {}
 export interface Player extends room.Player {}
 export interface Room extends room.Room {}
-
+export interface Move extends room.Move {}
 export interface OnGetOpponentPayload extends handlers.OnGetOpponentPayload {}
 export interface OnNewRoomPayload extends handlers.OnNewRoomPayload {}
 export interface OnReadyGamePayload extends handlers.OnReadyGamePayload {}
 export interface OnTeamSubmitPayload extends handlers.OnTeamSubmitPayload {}
+export interface OnActionProps extends handlers.OnActionProps {}
+export interface ResolveTurnPayload extends handlers.ResolveTurnPayload {}
+export interface Update extends handlers.Update {}
