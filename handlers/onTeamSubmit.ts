0cb3177a21ea568f58de3f45ec5a81ca93ea6506
Adib Khan <adib.khan@gmail.com>
2020-12-16 14:02:18 -0500

    add player props

--- handlers/onTeamSubmit.ts
+++ handlers/onTeamSubmit.ts
@@ -21,7 +21,8 @@ function onTeamSubmit(id: string, payload: OnTeamSubmitPayload) {
       }
       currentRoom.players[i]!.current = {
         team: currentTeam,
-        ready: false
+        ready: false,
+        active: 0
       }
 
       console.info(`Player ${id} is ready in room ${room}.`);
