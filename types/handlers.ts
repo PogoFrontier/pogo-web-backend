f8210fa8c27708659d3a15d36d5dee2cc5262470
Adib Khan <adib.khan@gmail.com>
2020-12-16 14:01:36 -0500

    add on action types

--- types/handlers.ts
+++ types/handlers.ts
@@ -17,3 +17,20 @@ export interface OnTeamSubmitPayload {
   room: string,
   team: TeamMember[]
 }
+
+export interface OnActionProps {
+  id: string,
+  room: string,
+  data: string
+}
+
+export interface Update {
+  id: string,
+  active: number,
+  hp?: number,
+}
+
+export interface ResolveTurnPayload {
+  time: number,
+  update: [Update | null, Update | null]
+}
