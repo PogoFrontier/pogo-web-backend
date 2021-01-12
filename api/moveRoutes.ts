417efee12abf3c4d45b7d740760baa7d0bfea094
Adib Khan <adib.khan@gmail.com>
2021-01-12 00:06:04 -0500

    add moves/team route

--- api/moveRoutes.ts
+++ api/moveRoutes.ts
@@ -8,10 +8,30 @@ const router = e.Router();
 // @route GET /api/moves/:id
 // @access Public (for now)
 router.get('/:id', (req, res) => {
-    try{
+    try {
         const result: any = moves[req.params.id];
         result ? res.json(result) : res.status(404).json(`Could not find move of id: ${req.params.id}`);
-    }catch(err){
+    } catch (err) {
+        console.error();
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.get('/team/:array', (req, res) => {
+    try {
+        const ids: string[] = req.params.array.split(",");
+        let arr = [];
+        for (const id of ids) {
+            const move = moves[id];
+            if (move) {
+                arr.push(move);
+            } else {
+                res.status(404).json(`Could not find move of id: ${id}`);
+                return;
+            }
+        }
+        res.json(arr);
+    } catch (err) {
         console.error();
         res.status(500).json({message: "Internal server error"});
     }
