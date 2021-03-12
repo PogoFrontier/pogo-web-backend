5ae7bd3ea5e0836057a42b9776e6f690621b8a31
Adib Khan <adib.khan@gmail.com>
2021-03-12 01:16:28 -0500

    add validate pokemon endpoint

--- api/pokemonRoutes.ts
+++ api/pokemonRoutes.ts
@@ -1,7 +1,10 @@
 import e from "express";
 import p from "../data/pokemon.json";
+import r from "../data/rules.json";
+import onTeamValidate from "../handlers/onTeamValidate";
 
 const pokemon: any = p;
+const rules: any = r;
 const router = e.Router();
 
 // @desc Get an array of Pokemon names to be used for the search feature
@@ -27,6 +30,30 @@ router.get('/:id', (req, res) => {
         result ? res.json(result) : res.status(404).json(`Could not find Pokemon of id: ${req.params.id}`);
     }catch(err){
         console.error();
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+// @desc Validate a Pokemon team
+// @route GET /api/validate/:team/:meta
+// @access Public (for now)
+router.get('/validate/:team/:meta', (req, res) => {
+    try{
+        const meta: any = rules[req.params.meta];
+        if (meta) {
+            const team = JSON.parse(req.params.team)
+            const data = onTeamValidate(team, meta)
+            if (typeof data === 'string') {
+                res.json({
+                    message: data
+                })
+            } else {
+                res.json(data)
+            }
+        } else {
+            res.status(404).json(`${req.params.id} is not a valid meta.`);
+        }
+    } catch(err) {
         res.status(500).json({message: "Internal server error"});
     }
 });
