4e760d3af117b5b1d6ac93ec9a95e94281031f12
1522784 <florian.kutz@t-online.de>
2021-04-06 17:54:11 +0200

    feat: delete rooms after some time

--- actions/to.ts
+++ actions/to.ts
@@ -12,7 +12,15 @@ function to(room: string, data: string, id?: string) {
         wait: currentRoom.wait,
         timerId: currentRoom.timerId,
         charge: currentRoom.charge
-    }));
+    }), (err) => {
+      if (err) {
+          console.error(err);
+          return;
+      }
+
+      // Let redis destroy room after one hour. This is only neccessary if the server is restarted before the battle is finished.
+      storeClient.expire("room:" + room, 60 * 60);
+  });
 
     for (let player of rooms.get(room)!.players) {
       if (player && player.id !== id) {
