0c88a5a97674a65119e7fd4968eaa01cfb833089
1522784 <florian.kutz@t-online.de>
2021-05-23 10:10:25 +0200

    feat: add venture cup

--- actions/getTranslation.ts
+++ actions/getTranslation.ts
@@ -17,12 +17,11 @@ async function getTranslation(key_id : string) {
 async function getStrings(lang :string){
     const res = await axios.get(`https://d1bbfbaqrr54l0.cloudfront.net/locale/${lang}.json`)
     if (res.data) {
-    const d: any = {}
-    for (const key of Object.keys(res.data)) {
-        d[key] = res.data[key]
-    }
-    console.log(d)
-    return d
+        const d: any = {}
+        for (const key of Object.keys(res.data)) {
+            d[key] = res.data[key]
+        }
+        return d
     }
 }
 
