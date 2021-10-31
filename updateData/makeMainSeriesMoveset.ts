53d1e1bfdf0084639074ee4bd44b8ec0a11bd09a
FlorianKutz <florian.kutz@t-online.de>
2021-10-31 16:39:26 +0100

    refactor: disabl unlogical wbs in main series

--- updateData/makeMainSeriesMoveset.ts
+++ updateData/makeMainSeriesMoveset.ts
@@ -3,125 +3,33 @@ import p from "../data/pokemon.json";
 import { Pokedex as dex } from "../data/pokedex";
 import m from "../data/moves.json";
 import { Learnsets } from '../data/learnsets'
+import { Pokedex } from '../data/pokedex'
 
 let pokemonList: any = p
 let localMoves: any = m;
 
 const hpTypes = ['bug', 'dark', 'dragon', 'electric', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water']
-const wbTypes = ['fire', 'ice', 'rock', "normal", 'water']
+var keyCache = new Map();
+var dexkeyCache = new Map();
 
 for(let id of Object.keys(pokemonList)) {
     let pokemon = pokemonList[id];
-
-    // Adapt key to match pokemon showdown
-    let key: string= pokemon.speciesId.replace("shadow", "").replace("therian", "").replace("incarnate", "").replace("galarian", "galar").replace("alolan", "alola").replace("_zen", "").replace("_standard", "").replace("female", "f").replace("male", "m").replace("_mega", "").replace(/_/g, "")
-    if(key.startsWith("charizard")) {
-        key = "charizard";
-    }
-    if(key.startsWith("pikachu")) {
-        key = "pikachu";
-    }
-    if(key.startsWith("mewtwo")) {
-        key = "mewtwo";
-    }
-    if(key.startsWith("castform")) {
-        key = "castform";
-    }
-    if(key.startsWith("deoxys")) {
-        key = "deoxys";
-    }
-    if(key.startsWith("shellos")) {
-        key = "shellos";
-    }
-    if(key.startsWith("gastrodon")) {
-        key = "gastrodon";
-    }
-    if(key.startsWith("burmy")) {
-        key = "burmy";
-    }
-    if(key.startsWith("wormadamplant")) {
-        key = "wormadam";
-    }
-    if(key.startsWith("cherrim")) {
-        key = "cherrim";
-    }
-    if(key.startsWith("giratina")) {
-        key = "giratina";
-    }
-    if(key.startsWith("shaymin")) {
-        key = "shaymin";
-    }
-    if(key.startsWith("arceus")) {
-        key = "arceus";
-    }
-    if(key.startsWith("keldeo")) {
-        key = "keldeo";
-    }
-    if(key.startsWith("genesect")) {
-        key = "genesect";
-    }
-    if(key.startsWith("meloetta")) {
-        key = "meloetta";
-    }
-    if(key.startsWith("pyroar")) {
-        key = "pyroar";
-    }
-    if (key.startsWith("pumpkaboo")) {
-        key = "pumpkaboo";
-    }
-    if (key.startsWith("gourgeist")) {
-        key = "gourgeist";
-    }
-    if(key.startsWith("hoopa")) {
-        key = "hoopa";
-    }
-    if(key.startsWith("toxtricity")) {
-        key = "toxtricity";
-    }
-    if(key.startsWith("zacian")) {
-        key = "zacian";
-    }
-    if(key.startsWith("zamazenta")) {
-        key = "zamazenta";
-    }
-    if(key.startsWith("sinistea")) {
-        key = "sinistea";
-    }
-    if(key.startsWith("polteageist")) {
-        key = "polteageist";
-    }
-    if(key.startsWith("calyrex")) {
-        key = "calyrex";
-    }
-    if(key.startsWith("eiscue")) {
-        key = "eiscue";
-    }
-    if(key.startsWith("indeedee")) {
-        key = "indeedee";
-    }
-    if(key.startsWith("morpeko")) {
-        key = "morpeko";
-    }
-    if(key.startsWith("eternatus")) {
-        key = "eternatus";
-    }
-    if(key.startsWith("urshifu")) {
-        key = "urshifu";
-    }
     
     let collectedQuickmoves: string[] = []
     let collectedChargemoves: string[] = []
 
+    let speciesId = pokemon.speciesId;
     while(true) {
-        const { quickmoves, chargemoves } = getLearnsetsOfKey(key, collectedQuickmoves, collectedChargemoves)
+        const { quickmoves, chargemoves } = getLearnset(speciesId, collectedQuickmoves, collectedChargemoves)
         collectedQuickmoves = quickmoves
         collectedChargemoves = chargemoves
 
+        let key = getLearnSetKey(speciesId);
         const origin = dex[key].prevo || dex[key].baseSpecies
         if(!origin || ["Galar", "Alola"].includes(dex[key].forme) && !dex[key].prevo) {
             break;
         }
-        key = origin.toLowerCase().replace("-", "").replace(".", "").replace(" ", "").replace(":", "").replace("é", "e").replace("é", "e").replace("’", "")
+        speciesId = origin.toLowerCase().replace("-", "").replace(".", "").replace(" ", "").replace(":", "").replace("é", "e").replace("é", "e").replace("’", "");
     }
 
     // Add special moves that don't appear in learnsets
@@ -160,11 +68,141 @@ for(let id of Object.keys(pokemonList)) {
     delete pokemon.eliteMoves
 }
 
-function getLearnsetsOfKey(key: string, quickmoves: string[], chargemoves: string[]) {
+function getPokedexKey(speciesId: string): string {
+    if(dexkeyCache.has(speciesId)) {
+        return dexkeyCache.get(speciesId);
+    }
+
+    let key: string = speciesId.replace("shadow", "").replace("overcast", "").replace("altered", "").replace("sky", "").replace("incarnate", "").replace("galarian", "galar").replace("alolan", "alola").replace("_standard", "").replace("female", "f").replace("male", "m").replace(/_/g, "")
+
+    if (key.startsWith("pikachu")) {
+        key = "pikachu";
+    }
+    if (key.startsWith("shellos")) {
+        key = "shellos";
+    }
+    if (key.startsWith("gastrodon")) {
+        key = "gastrodon";
+    }
+    if (key.startsWith("burmy")) {
+        key = "burmy";
+    }
+    if (key.startsWith("wormadamplant")) {
+        key = "wormadam";
+    }
+    if (key.startsWith("cherrimsunny")) {
+        key = "cherrimsunshine";
+    }
+    if (key.startsWith("arceus")) {
+        key = "arceus";
+    }
+    if (key.startsWith("keldeo")) {
+        key = "keldeo";
+    }
+    if (key.startsWith("meloetta")) {
+        key = "meloetta";
+    }
+    if (key.startsWith("pyroar")) {
+        key = "pyroar";
+    }
+    if (key.startsWith("pumpkaboo")) {
+        key = "pumpkaboo";
+    }
+    if (key.startsWith("gourgeist")) {
+        key = "gourgeist";
+    }
+    if (key.startsWith("toxtricity")) {
+        key = "toxtricity";
+    }
+    if (key.startsWith("zacian")) {
+        key = "zacian";
+    }
+    if (key.startsWith("zamazenta")) {
+        key = "zamazenta";
+    }
+    if (key.startsWith("sinistea")) {
+        key = "sinistea";
+    }
+    if (key.startsWith("polteageist")) {
+        key = "polteageist";
+    }
+    if (key.startsWith("calyrex")) {
+        key = "calyrex";
+    }
+    if (key.startsWith("indeedee")) {
+        key = "indeedee";
+    }
+    if (key.startsWith("morpeko")) {
+        key = "morpeko";
+    }
+    if (key.startsWith("eternatus")) {
+        key = "eternatus";
+    }
+    if (key.startsWith("urshifu")) {
+        key = "urshifu";
+    }
+    if (key.startsWith("eiscue")) {
+        key = "eiscue";
+    }
+
+    dexkeyCache.set(speciesId, key);
+    return key;
+}
+
+function getLearnSetKey(speciesId: string): string {
+    // Read from cache
+    if(keyCache.has(speciesId)) {
+        return keyCache.get(speciesId);
+    }
+
+    // Trim speciesId to match Pokémon Showdown
+    let key = getPokedexKey(speciesId);
+    key = key.replace("therian", "")
+    if (!["zamazenta"].includes(key)) {
+        key = key.replace("zen", "")
+    }
+    if (!["mrmimegalar", "meganium", "yanmega"].includes(key)) {
+        key = key.replace("mega", "")
+    }
+    if (key.startsWith("charizard")) {
+        key = "charizard";
+    }
+    if (key.startsWith("mewtwo")) {
+        key = "mewtwo";
+    }
+    if(key.startsWith("castform")) {
+        key = "castform";
+    }
+    if (key.startsWith("cherrim")) {
+        key = "cherrim";
+    }
+    if (key.startsWith("deoxys")) {
+        key = "deoxys";
+    }
+    if (key.startsWith("giratina")) {
+        key = "giratina";
+    }
+    if (key.startsWith("shaymin")) {
+        key = "shaymin";
+    }
+    if (key.startsWith("genesect")) {
+        key = "genesect";
+    }
+    if (key.startsWith("hoopa")) {
+        key = "hoopa";
+    }
+
+    // Save progress and return
+    keyCache.set(speciesId, key);
+    return key;
+}
+
+function getLearnset(speciesId: string, quickmoves: string[], chargemoves: string[]) {
+    let key = getLearnSetKey(speciesId);
 
     let pokeWithLearnset = Learnsets[key];
     if (!pokeWithLearnset || !pokeWithLearnset.learnset) {
-        console.error("Unidentified pokemon " + key);
+        console.error("Unidentified pokemon " + key + ". SpeciesId: " + speciesId);
         process.exit(1);
     }
 
@@ -183,11 +221,11 @@ function getLearnsetsOfKey(key: string, quickmoves: string[], chargemoves: strin
             }
             continue;
         } else if (move === "weatherball") {
-            if (chargemoves.includes("WEATHER_BALL_NORMAL")) {
+            if (chargemoves.some(chargemove => chargemove.startsWith("WEATHER_BALL"))) {
                 continue
             }
-            for (let type of wbTypes) {
-                chargemoves.push("WEATHER_BALL_" + type.toUpperCase())
+            for (let wb of getWeatherBalls(speciesId)) {
+                chargemoves.push(wb);
             }
             continue;
         }
@@ -208,6 +246,50 @@ function getLearnsetsOfKey(key: string, quickmoves: string[], chargemoves: strin
         quickmoves,
         chargemoves
     }
+}
+
+function getWeatherBalls(speciesId: string): string[] {
+    if(speciesId.endsWith("_sunny")) {
+        return ["WEATHER_BALL_FIRE"];
+    }
+    if (speciesId.endsWith("_rainy")) {
+        return ["WEATHER_BALL_WATER"];
+    }
+    if (speciesId.endsWith("_snowy")) {
+        return ["WEATHER_BALL_ICE"];
+    }
+
+    const key = getPokedexKey(speciesId)
+    if (!Pokedex[key]?.abilities) {
+        console.error("Unidentified pokemon for weather balls " + key + ". SpeciesId: " + speciesId);
+        process.exit(1);
+    }
+    const abilities: string[] = Object.values(Pokedex[key].abilities);
+    let weatherBalls: string[] = [];
+
+    for (const ability of abilities) {
+        if (!weatherBalls.includes("WEATHER_BALL_FIRE") && ["Chlorophyll", "Desolate Land", "Drought", "Harvest", "Leaf Guard", "Solar Power"].includes(ability)) {
+            weatherBalls.push("WEATHER_BALL_FIRE")
+        }
+        if (!weatherBalls.includes("WEATHER_BALL_ICE") && ["Ice Body", "Ice Face", "Slush Rush", "Snow Cloak", "Snow Warning"].includes(ability)) {
+            weatherBalls.push("WEATHER_BALL_ICE")
+        }
+        if (!weatherBalls.includes("WEATHER_BALL_NORMAL") && ["Air Lock", "Cloud Nine", "Delta Stream", "Flower Gift", "Forecast"].includes(ability)) {
+            weatherBalls.push("WEATHER_BALL_NORMAL")
+        }
+        if (!weatherBalls.includes("WEATHER_BALL_ROCK") && ["Flower Gift", "Forecast", "Sand Force", "Sand Rush", "Sand Spit", "Sand Stream", "Sand Veil"].includes(ability)) {
+            weatherBalls.push("WEATHER_BALL_ROCK")
+        }
+        if (!weatherBalls.includes("WEATHER_BALL_WATER") && ["Drizzle", "Hydration", "Primordial Sea", "Rain Dish", "Swift Swim"].includes(ability)) {
+            weatherBalls.push("WEATHER_BALL_WATER")
+        }
+    }
+
+    if(!weatherBalls.length) {
+        weatherBalls.push("WEATHER_BALL_NORMAL");
+    }
+
+    return weatherBalls
 }
 
 fs.writeFileSync("data/pokemonWithMainSeriesMoves.json", JSON.stringify(pokemonList, null, 2))
